2cdef39e6d2554feff09a4a2e65d31cff7719c30
Jacob George <jakergeorge@gmail.com>
2016-12-04 16:21:50 -0500

    added some basic search functionality in search route

---
+++ goodtracks/routes/search.js
@@ -0,0 +1,50 @@
+//express
+var express = require('express');
+var router = express.Router();
+
+//database
+var mongoose = require('mongoose');
+var sanitize = require('mongo-sanitize');
+
+//schemas
+Song = mongoose.model('Song');
+Album = mongoose.model('Album');
+
+//TODO: doing search of albums and songs separately because searching two collections is very inefficient, can easily change later
+router.post("/search-songs", function(req, res) {
+   var query = req.body.searchQuery;
+   query = sanitize(query.trim());
+
+   Song.find(
+       { $text : { $search : query } },
+       { score : { $meta: "textScore" } }
+   )
+       .sort({ score : { $meta : 'textScore' } })
+       .exec(function(err, songs) {
+           if(!err) {
+               res.render('search', {isSong: true, songs: songs});
+           } else {
+               res.render('error', {message: "There was a problem searching for that song.", error: err});
+           }
+       });
+
+
+});
+
+router.post("/search-albums", function(req, res) {
+   var query = req.body.searchQuery;
+   query = sanitize(query.trim());
+
+   Album.find({ $text : { $search : query } },
+       { score : { $meta: "textScore" } }
+   )
+       .sort({ score : { $meta : 'textScore' } })
+       .exec(function(err, albums) {
+           if(!err) {
+               res.render('search', {isSong: false, albums: albums});
+           } else {
+               res.render('error', {message: "There was a problem searching for that album.", error: err});
+           }
+       });
+
+});
