6b5d584a67b7d044651f622ad0e2024d5c1f1d56
Jacob George <jakergeorge@gmail.com>
2016-12-04 14:42:16 -0500

    added some views

--- goodtracks/db.js
+++ goodtracks/db.js
@@ -1,4 +1,10 @@
 var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
 
+
+var User = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+
+})
 mongoose.connect("//////////");
