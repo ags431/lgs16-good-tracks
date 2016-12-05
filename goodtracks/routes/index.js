2e2dd1941cc76f78ba1f657d60ed946063354e6a
August Sun <august99us@debianAugust.aug>
2016-12-05 13:31:05 -0500

    fastforward preparation

--- goodtracks/routes/index.js
+++ goodtracks/routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/playlist', function(req, res, next) {
+  // TODO: Logic to construct and render playlist page
+}
+
+router.get('/wishlist', function(req, res, next) {
+  // TODO: Logic to construct and render wishlist page
+}
+
+router.get('/listento', function(req, res, next) {
+  // TODO: Logic to construct and render listento page
+}
+
+router.get('/song', function(req, res, next) {
+  songid = req.query.id;
+  // TODO: Logic to construct and render song page
+}
+
+router.get('/album', function(req, res, next) {
+  albumid = req.query.id;
+  // TODO: Logic to construct and render album page
+}
+
 module.exports = router;
