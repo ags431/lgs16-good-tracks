fb231911c5e27ca9a632b6b02d590a5d29772bb1
Jacob George <jakergeorge@gmail.com>
2016-12-04 22:13:53 -0500

    fixed form

--- goodtracks/db.js
+++ goodtracks/db.js
@@ -4,7 +4,13 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new mongoose.Schema({
     firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
+    lastName: {type: String, required: true}
+    /*
+       Playlist, wishlist, listening to
+     */
+});
 
-})
+/*
+* Schema for Song, Album required as well
+ */
 mongoose.connect("//////////");
--- goodtracks/routes/index.js
+++ goodtracks/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/add', function(req, res) {
+  res.render('add');
+});
+
+router.post('/add', function(req, res) {
+
+});
+
 module.exports = router;
